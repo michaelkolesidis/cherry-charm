dd2dc5c709e2e3b0f234ac9e56b478a3ccdc532b
Michael Kolesidis <michael.kolesidis@gmail.com>
2025-07-07 19:17:24 +0300

    update dependencies

--- src/lights/Lights.tsx
+++ src/lights/Lights.tsx
@@ -17,9 +17,9 @@
 export default function Lights() {
   return (
     <>
-      <directionalLight position={[-2, 3, 2]} intensity={1} />
-      <directionalLight position={[2, 3, 2]} intensity={1} />
-      <ambientLight intensity={0.5} />
+      <directionalLight position={[-2, 3, 2]} intensity={2} />
+      <directionalLight position={[2, 3, 2]} intensity={2} />
+      <ambientLight intensity={3} />
     </>
   );
 }
