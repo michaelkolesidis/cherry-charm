db40d457cfd1174ff334d877df4886d6cdf03ccb
Michael Kolesidis <michael.kolesidis@gmail.com>
2026-04-18 03:41:37 +0300

    relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   server: {
     host: true,
