91fd76e0ca2f95d9ec93d3222f737166d3f1221c
Michael Kolesidis <michael.kolesidis@gmail.com>
2026-04-19 14:10:04 +0300

    thumbfeed

--- src/hooks/useAnimatedNumber.tsx
+++ src/hooks/useAnimatedNumber.tsx
@@ -1,24 +1,8 @@
-/*
- *  Copyright (c) Michael Kolesidis <michael.kolesidis@gmail.com>
- *  GNU Affero General Public License v3.0
- *
- *  ATTENTION! FREE SOFTWARE
- *  This website is free software (free as in freedom).
- *  If you use any part of this code, you must make your entire project's source code
- *  publicly available under the same license. This applies whether you modify the code
- *  or use it as it is in your own project. This ensures that all modifications and
- *  derivative works remain free software, so that everyone can benefit.
- *  If you are not willing to comply with these terms, you must refrain from using any part of this code.
- *
- *  For full license terms and conditions, you can read the AGPL-3.0 here:
- *  https://www.gnu.org/licenses/agpl-3.0.html
- */
-
 import { useEffect, useRef, useState } from 'react';
 
 export default function useAnimatedNumber(
   target: number,
-  duration: number = 1000
+  duration: number = 1000,
 ) {
   const [animatedValue, setAnimatedValue] = useState(target);
   const animationFrameId = useRef<number | null>(null);
--- src/interface/Interface.tsx
+++ src/interface/Interface.tsx
@@ -1,19 +1,3 @@
-/*
- *  Copyright (c) Michael Kolesidis <michael.kolesidis@gmail.com>
- *  GNU Affero General Public License v3.0
- *
- *  ATTENTION! FREE SOFTWARE
- *  This website is free software (free as in freedom).
- *  If you use any part of this code, you must make your entire project's source code
- *  publicly available under the same license. This applies whether you modify the code
- *  or use it as it is in your own project. This ensures that all modifications and
- *  derivative works remain free software, so that everyone can benefit.
- *  If you are not willing to comply with these terms, you must refrain from using any part of this code.
- *
- *  For full license terms and conditions, you can read the AGPL-3.0 here:
- *  https://www.gnu.org/licenses/agpl-3.0.html
- */
-
 import useGame from '../stores/store';
 import HelpModal from './helpModal/HelpModal';
 import HelpButton from './helpButton/HelpButton';
@@ -32,6 +16,22 @@ const Interface = () => {
 
       {/* Modal */}
       {modal && <HelpModal />}
+
+      {/* Thumbfeed logo */}
+      <a href="https://thumbfeed.com" className="site-logo">
+        <img
+          id="desktop-logo"
+          className="site-logo-img"
+          src="thumbfeed-logo-inline.svg"
+          alt="Thumbfeed logo"
+        />
+        <img
+          id="mobile-logo"
+          className="site-logo-img"
+          src="thumbfeed-logo.svg"
+          alt="Thumbfeed logo"
+        />
+      </a>
 
       {/* Logo */}
       <div id="logo-section">
